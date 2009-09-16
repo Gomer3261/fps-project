59238f6e7c8eafcf548b5f6df0a16a8c9cf3118d
chasemoskal <chasemoskal@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2009-09-16 02:57:12 +0000

    Added the WIP newProg for Moguri to check out.

---
+++ trunk/gamedata/newProg/modules/gamesystems/mousetools.py
@@ -0,0 +1,129 @@
+################################
+### ------ MouseTools ------ ###
+################################
+### Copyright 2009 Chase Moskal!
+# This contains MOUSE, for mouse movement related stuff,
+# CURSOR, for simple cursor stuff,
+# and MOUSEBUTTONS, for mouseclicks and stuff.
+# lots of stuff.
+
+INIT = 0
+
+mouse = None
+cursor = None
+
+def init(mousemove, mouseover):
+	"""Init the module:	This requires a mouse movement sensor and a mouse over sensor"""
+		
+	global mouse, cursor, INIT
+		
+	mouse = MOUSE(mousemove)
+	cursor = CURSOR(mouseover)
+	INIT = 1
+
+##############################
+### ------  MOUSE   ------ ###
+##############################
+# For getting Mouse movement information.
+
+class MOUSE:
+    import math
+    import Rasterizer
+
+    def __init__(self, mousemove):
+        self.mousemove = mousemove
+
+        self.width = self.Rasterizer.getWindowWidth()
+        self.height = self.Rasterizer.getWindowHeight()
+
+        self.centerX = self.width/2
+        self.centerY = self.height/2
+
+
+    def reset(self):
+        self.Rasterizer.setMousePosition(self.centerX, self.centerY)
+
+
+    def show(self, vis=1):
+        self.Rasterizer.showMouse(vis)
+
+    def hide(self, vis=0):
+        self.Rasterizer.showMouse(vis)
+
+    def getPosition(self):
+        position = self.mousemove.position
+
+        X = position[0]
+        Y = position[1]
+
+        return X, Y
+
+
+    def isAtCenter(self):
+        X, Y = self.getPosition()
+        center = 1
+
+        if X != self.centerX:
+            center = 0
+
+        if Y != self.centerY:
+            center = 0
+
+        return center
+
+
+    def getPositionFromCenter(self):
+        X, Y = self.getPosition()
+
+        X = (X-self.centerX)
+        Y = (self.centerY-Y)
+
+        return X, Y
+
+    def isPositive(self):
+        if self.mousemove.positive:
+            return 1
+        else:
+            return 0
+
+    def getMovement(self):
+        if self.isPositive():
+            X, Y = self.getPositionFromCenter()
+        else:
+            X = 0
+            Y = 0
+        if X or Y:
+            self.reset()
+        return X, Y
+
+
+
+####################################
+### ------	CURSOR	  ------ ###
+####################################
+# For getting cursor information, position,
+# object hover...
+
+class CURSOR:
+    import Rasterizer
+    
+    def __init__(self, mouseover):
+        self.mouseover = mouseover
+    
+    def getPosition(self):
+        if self.mouseover.positive:
+            pos = self.mouseover.hitPosition
+            return pos
+        return None
+
+    def getRaisedPosition(self, a=0.5):
+        position = self.getPosition()
+        if position:
+            position[2] += a
+        return position
+    
+    def getHitObject(self):
+        if self.mouseover.isPositive():
+            obj = self.mouseover.getHitObject()
+            return obj
+        return None
---
+++ trunk/gamedata/newProg/modules/interface/options.py
@@ -0,0 +1,196 @@
+#############################
+### ------ OPTIONS ------ ###
+#############################
+### Copyright 2009 Chase Moskal!
+# This object handles the saving/loading of options.
+
+INIT = 0
+
+import traceback
+
+sepchar = "\n=------ Settings Above / Controls Below ------=\n\n"
+path = "FPS_options.txt"
+settings = {}
+controls = {}
+
+def init():
+	"""Init the module."""
+	
+	global INIT
+	
+	load()
+	INIT = 1
+
+
+
+
+
+
+
+def saveDefaults():
+    global sepchar
+    global path
+    global settings
+    global controls
+    global traceback
+    
+    try:
+        settings = {}
+        controls = {}
+
+        # SETTINGS
+        settings["mxsens"] = 5.0
+        settings["mysens"] = 5.0
+        settings["inverty"] = 0
+        settings["invertx"] = 0
+
+        # CONTROLS
+        controls["spawn"] = "space-key"
+        controls["suicide"] = "del-key"
+
+        controls["forward"] = "w-key"
+        controls["backward"] = "s-key"
+        controls["left"] = "a-key"
+        controls["right"] = "d-key"
+        controls["jump"] = "space-key"
+        controls["sprint"] = "leftshift-key"
+
+        controls["use"] = "lmb"
+        controls["aim"] = "rmb"
+        controls["reload"] = "r-key"
+
+        controls["interact"] = "e-key"
+
+        controls["menu"] = "tab-key"
+
+        
+        
+        # Requesting a SAVE operation
+        result = save()
+        
+        return result
+    except:
+        traceback.print_exc()
+    
+def setSetting(key, value):
+    global sepchar
+    global path
+    global settings
+    global controls
+    global traceback
+
+    key = key.lower()
+    
+    settings[key] = value
+    r = save()
+    return r
+
+def setControl(key, value):
+    global sepchar
+    global path
+    global settings
+    global controls
+    global traceback
+
+    key = key.lower()
+    
+    controls[key] = value
+    r = save()
+    return r
+
+def save():
+    global sepchar
+    global path
+    global settings
+    global controls
+    global traceback
+
+    import os
+    
+    try:
+        settingsfile = "// This is the options file for the FPS Project. You can reset these options to default by typing \"options default\" in the in-game terminal." + os.linesep + os.linesep
+        for key in settings:
+            settingsfile += key + ": " + repr(settings[key]) + os.linesep
+
+        controlsfile = ""
+        for key in controls:
+            controlsfile += key + ": " + repr(controls[key]) + os.linesep
+
+        nativesepchar = sepchar.replace("\n", os.linesep)
+        newfile = settingsfile + nativesepchar + controlsfile
+        
+        f = open(path, "w")
+        f.write(newfile)
+        f.close()
+
+        print("Options Saved")
+
+        import inputs
+        if inputs.INIT:
+            inputs.controller.setControls(controls)
+    
+        return 1
+    except:
+        traceback.print_exc()
+        print "\n\n", settings, "\n\n", controls
+        return 0
+
+def load():
+    global sepchar
+    global path
+    global settings
+    global controls
+    global traceback
+    
+    try:
+        f = open(path, "r")
+        data = f.read()
+        f.close()
+
+        # Convert to clean
+        data = data.replace("\r\n", "\n")
+        data = data.replace("\r", "\n")
+
+        # split into parts
+        parts = data.split(sepchar)
+
+        settingsfile = parts[0]
+        controlsfile = parts[1]
+
+        # Settings
+        lines = settingsfile.split("\n")
+        statements = {}
+        for line in lines:
+            if line and not (line.find("//") != -1):
+                parts = line.split(":")
+                name = parts[0].strip().lower()
+                value = parts[1].strip()
+                statements[name] = eval(value)
+        settings = statements
+
+        # Controls
+        lines = controlsfile.split("\n")
+        statements = {}
+        for line in lines:
+            if line and not (line.find("//") != -1):
+                parts = line.split(":")
+                name = parts[0].strip().lower()
+                value = parts[1].strip()
+                statements[name] = eval(value)
+        controls = statements
+
+        print "Options Loaded"
+
+        import inputs
+        if inputs.INIT:
+            controller.setControls(controls)
+        
+        return 1
+    except:
+        traceback.print_exc()
+        print "Error loading; will attempt to save defaults and use those"
+        result = saveDefaults()
+        if result:
+            return 2 # This means the load failed, but the saving of defaults worked.
+        return 0
+
