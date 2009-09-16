23ed7f11e4203e960c5d374f7d71e4ee63dbed03
Geoff.Gollmer <Geoff.Gollmer@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2009-09-16 00:49:15 +0000

    Just fixing some of my syntax according to the new standards.

--- gamedata/prog/modules/domain.py
+++ gamedata/prog/modules/domain.py
@@ -21,24 +21,24 @@ class FACE:
     
     sides = 4
 
-    extraheight = 10
+    extraHeight = 10
     height = [0, 10]
 
-    leftside = []
-    rightside = []
+    leftSide = []
+    rightSide = []
     top = []
     bottom = []
     
     def __init__(self, vertices, height=10):
         self.vertices = vertices
-        self.extraheight = height
+        self.extraHeight = height
         self.calcSides()
 
     def quickContains(self, position):
         #quick contains checks if the position is within the highest and lowest values on the x any y axis
         #essentially using a bounding box for the calculation, which should speed up the search dramatically.
         
-        orgverts = self.orgverts
+        orgVerts = self.orgVerts
         verts = self.vertices
 
         #this detects if your object is within the height range, before anything else is calculated. It's a nice calculation saver when it's false.
@@ -47,13 +47,13 @@ class FACE:
 
         #4 sided polygon bounding box search.
         if len(self.vertices) == 4:
-            if position[0] >= verts[orgverts[0][3]][0] and position[0] <= verts[orgverts[0][0]][0] and position[1] >= verts[orgverts[1][3]][1] and position[1] <= verts[orgverts[1][0]][1]:
+            if position[0] >= verts[orgVerts[0][3]][0] and position[0] <= verts[orgVerts[0][0]][0] and position[1] >= verts[orgVerts[1][3]][1] and position[1] <= verts[orgVerts[1][0]][1]:
                 return 1
             else:
                 return 0
         #3 sided polygon bounding box search.
         else:
-            if position[0] >= verts[orgverts[0][2]][0] and position[0] <= verts[orgverts[0][0]][0] and position[1] >= verts[orgverts[1][2]][1] and position[1] <= verts[orgverts[1][0]][1]:
+            if position[0] >= verts[orgVerts[0][2]][0] and position[0] <= verts[orgVerts[0][0]][0] and position[1] >= verts[orgVerts[1][2]][1] and position[1] <= verts[orgVerts[1][0]][1]:
                 return 1
             else:
                 return 0
@@ -62,8 +62,8 @@ class FACE:
     def contains(self, position):
         #this is the function you need to worry about after creating the object. No other function should ever need to be called.
         
-        toppoint = self.calcIntercept(self.top, position, 1)
-        bottompoint = self.calcIntercept(self.bottom, position, 1)
+        topPoint = self.calcIntercept(self.top, position, 1)
+        bottomPoint = self.calcIntercept(self.bottom, position, 1)
 
         #this detects if your object is within the height range, before anything else is calculated. It's a nice calculation saver when it's false.
         if position[2] < self.height[0] or position[2] > self.height[1]:
@@ -72,12 +72,12 @@ class FACE:
         #if it's a four sided polygon this finds if the point is inside the polygon.
         elif len(self.vertices) == 4:
 
-            rightpoint = self.calcIntercept(self.rightside, position, 0)
-            leftpoint = self.calcIntercept(self.leftside, position, 0)
+            rightPoint = self.calcIntercept(self.rightSide, position, 0)
+            leftPoint = self.calcIntercept(self.leftSide, position, 0)
 
             #checking if the position is between the interception points.
-            if (position[0] >= leftpoint[0] and position[0] <= rightpoint[0]) or (position[0] <= leftpoint[0] and position[0] >= rightpoint[0]):
-                if (position[1] <= toppoint[1] and position[1] >= bottompoint[1]) or (position[1] >= toppoint[1] and position[1] <= bottompoint[1]):
+            if (position[0] >= leftPoint[0] and position[0] <= rightPoint[0]) or (position[0] <= leftPoint[0] and position[0] >= rightPoint[0]):
+                if (position[1] <= topPoint[1] and position[1] >= bottomPoint[1]) or (position[1] >= topPoint[1] and position[1] <= bottomPoint[1]):
                     return 1
                 else:
                     return 0
@@ -86,11 +86,11 @@ class FACE:
         #this is for 3 sided polygons.
         else:
             #if it's a regular triangle
-            if self.rightside == []:
-                leftpoint = self.calcIntercept(self.leftside, position, 0)
+            if self.rightSide == []:
+                leftPoint = self.calcIntercept(self.leftSide, position, 0)
                 
-                if position[0] >= leftpoint[0]:
-                    if (position[1] <= toppoint[1] and position[1] >= bottompoint[1]) or (position[1] >= toppoint[1] and position[1] <= bottompoint[1]):
+                if position[0] >= leftPoint[0]:
+                    if (position[1] <= topPoint[1] and position[1] >= bottomPoint[1]) or (position[1] >= topPoint[1] and position[1] <= bottomPoint[1]):
                         return 1
                     else:
                         return 0
@@ -98,10 +98,10 @@ class FACE:
                     return 0
             #this is for when top or bottom end up vertical for one reason or another.
             else:
-                rightpoint = self.calcIntercept(self.rightside, position, 0)
+                rightpoint = self.calcIntercept(self.rightSide, position, 0)
 
-                if position[0] <= rightpoint[0]:
-                    if (position[1] <= toppoint[1] and position[1] >= bottompoint[1]) or (position[1] >= toppoint[1] and position[1] <= bottompoint[1]):
+                if position[0] <= rightPoint[0]:
+                    if (position[1] <= topPoint[1] and position[1] >= bottomPoint[1]) or (position[1] >= topPoint[1] and position[1] <= bottomPoint[1]):
                         return 1
                     else:
                         return 0
@@ -121,89 +121,89 @@ class FACE:
         #otherwise use slope y-intercept to figure it out.
         else:
             slope = (side[1][1]-side[0][1])/(side[1][0]-side[0][0])
-            yint = side[0][1] + ((0 - side[0][0]) * slope)
+            yInt = side[0][1] + ((0 - side[0][0]) * slope)
             #if we are calculating for x
             if direction == 0:
-                xpos = (position[1]-yint)/slope
-                return [xpos, position[1], position[2]]
+                xPos = (position[1]-yInt)/slope
+                return [xPos, position[1], position[2]]
             #if we are calculating for y
             else:
-                ypos = (slope*position[0]) + yint
-                return [position[0], ypos, position[2]]
+                yPos = (slope*position[0]) + yInt
+                return [position[0], yPos, position[2]]
 
             
 
-    def sortVerts(self, orgverts, pos):
+    def sortVerts(self, orgVerts, pos):
         import operator
         #calculating verts according to position (orgverts is the vertice order pos is weither it's based on x or y co-ordinates)
         for i in range(len(self.vertices)):
-            orgverts[pos].append([self.vertices[i][pos], i])
+            orgVerts[pos].append([self.vertices[i][pos], i])
         
-        getcount = operator.itemgetter(0)
-        orgverts[pos] = sorted(orgverts[pos], key=getcount)
+        getCount = operator.itemgetter(0)
+        orgVerts[pos] = sorted(orgVerts[pos], key=getCount)
 
         #sorted sorts from lowest to highest, I wanted highest to lowest, so I reverse the list.
-        orgverts[pos].reverse()
+        orgCerts[pos].reverse()
 
         #trust me this needs to happen, deal with it.
         for i in range(len(self.vertices)):
-            orgverts[pos][i] = orgverts[pos][i][1]
+            orgVerts[pos][i] = orgVerts[pos][i][1]
         
-        return orgverts
+        return orgVerts
 
     
 
     def calcSides(self):
         verts = self.vertices
-        orgverts = [[], [], []]
-        #organizing verts left-right, top-bottom
-        orgverts = self.sortVerts(orgverts, 0)
-        orgverts = self.sortVerts(orgverts, 1)
-        orgverts = self.sortVerts(orgverts, 2)
+        orgVerts = [[], [], []]
+        #organizing verts left-right, top-bottom, high-low
+        orgVerts = self.sortVerts(orgVerts, 0)
+        orgVerts = self.sortVerts(orgVerts, 1)
+        orgVerts = self.sortVerts(orgVerts, 2)
 
-        self.orgverts = orgverts
+        self.orgVerts = orgVerts
 
         if len(self.vertices) == 4:
 
-            self.height = [verts[orgverts[2][3]][2], verts[orgverts[2][0]][2] + self.extraheight]
+            self.height = [verts[orgVerts[2][3]][2], verts[orgVerts[2][0]][2] + self.extraHeight]
             
             #calculating sides if the polygon is 4 sided.
-            self.top = [verts[orgverts[1][0]],verts[orgverts[1][1]]]
-            self.bottom = [verts[orgverts[1][3]],verts[orgverts[1][2]]]
+            self.top = [verts[orgVerts[1][0]],verts[orgVerts[1][1]]]
+            self.bottom = [verts[orgVerts[1][3]],verts[orgVerts[1][2]]]
 
-            if (orgverts[0][0] == orgverts[1][0] and orgverts[0][1] == orgverts[1][1]) or (orgverts[0][0] == orgverts[1][1] and orgverts[0][1] == orgverts[1][0]) or (orgverts[0][0] == orgverts[1][2] and orgverts[0][1] == orgverts[1][3]) or (orgverts[0][0] == orgverts[1][3] and orgverts[0][1] == orgverts[1][2]):
-                self.leftside = [verts[orgverts[0][3]],verts[orgverts[0][1]]]
-                self.rightside = [verts[orgverts[0][0]],verts[orgverts[0][2]]]
+            if (orgVerts[0][0] == orgVerts[1][0] and orgVerts[0][1] == orgVerts[1][1]) or (orgVerts[0][0] == orgVerts[1][1] and orgVerts[0][1] == orgVerts[1][0]) or (orgVerts[0][0] == orgVerts[1][2] and orgVerts[0][1] == orgVerts[1][3]) or (orgVerts[0][0] == orgVerts[1][3] and orgVerts[0][1] == orgVerts[1][2]):
+                self.leftSide = [verts[orgVerts[0][3]],verts[orgVerts[0][1]]]
+                self.rightSide = [verts[orgVerts[0][0]],verts[orgVerts[0][2]]]
 
             else:
-                self.leftside = [verts[orgverts[0][3]],verts[orgverts[0][2]]]
-                self.rightside = [verts[orgverts[0][0]],verts[orgverts[0][1]]]
+                self.leftSide = [verts[orgVerts[0][3]],verts[orgVerts[0][2]]]
+                self.rightSide = [verts[orgVerts[0][0]],verts[orgVerts[0][1]]]
 
 
         
         else:
 
-            self.height = [verts[orgverts[2][2]][2], verts[orgverts[2][0]][2] + self.extraheight]
+            self.height = [verts[orgVerts[2][2]][2], verts[orgVerts[2][0]][2] + self.extraHeight]
             
             #calculating sides if the polygon is 3 sided
-            self.rightside = [verts[orgverts[0][0]],verts[orgverts[0][1]]]
+            self.rightSide = [verts[orgVerts[0][0]],verts[orgVerts[0][1]]]
 
-            if verts[orgverts[0][0]][1] >= verts[orgverts[0][1]][1]:
-                self.top = [verts[orgverts[0][0]],verts[orgverts[0][2]]]
-                self.bottom = [verts[orgverts[0][1]],verts[orgverts[0][2]]]
+            if verts[orgVerts[0][0]][1] >= verts[orgVerts[0][1]][1]:
+                self.top = [verts[orgVerts[0][0]],verts[orgVerts[0][2]]]
+                self.bottom = [verts[orgVerts[0][1]],verts[orgVerts[0][2]]]
             else:
-                self.top = [verts[orgverts[0][1]],verts[orgverts[0][2]]]
-                self.bottom = [verts[orgverts[0][0]],verts[orgverts[0][2]]]
+                self.top = [verts[orgVerts[0][1]],verts[orgVerts[0][2]]]
+                self.bottom = [verts[orgVerts[0][0]],verts[orgVerts[0][2]]]
 
             #if top or bottom happen to be straight lines, it broke the script, this replaces the straight line with left side, and rightside becomes the replaced value.
             if self.top[0][0] == self.top[1][0]:
-                self.leftside = self.top
-                self.top = self.rightside
-                self.rightside = []
+                self.leftSide = self.top
+                self.top = self.rightSide
+                self.rightSide = []
 
             elif self.bottom[0][0] == self.bottom[1][0]:
-                self.leftside = self.bottom
-                self.bottom = self.rightside
+                self.leftSide = self.bottom
+                self.bottom = self.Rightside
                 self.rightside = []
 
 
@@ -227,10 +227,10 @@ class DOMAIN():
            
             poly = mesh.getPolygon(i)
             for v in range(poly.getNumVertex()):
-                vindex = poly.getVertexIndex(v)
-                vertex = mesh.getVertex(0, vindex)
-                vertpos = [vertex.x + obj.position[0], vertex.y + obj.position[1], vertex.z + obj.position[2]]
-                verts.append(vertpos)
+                vIndex = poly.getVertexIndex(v)
+                vertex = mesh.getVertex(0, vIndex)
+                vertPos = [vertex.x + obj.position[0], vertex.y + obj.position[1], vertex.z + obj.position[2]]
+                verts.append(vertPos)
             face = self.FACE(verts, height)
             self.faces.append(face)
 
@@ -285,14 +285,14 @@ def isIn(data, domain="", quick=0):
         else:
             return 0
     else:
-        domainlist = []
+        domainList = []
         for domain in domains:
             if quick == 0:
                 if domains[domain].contains(position) == 1:
-                    domainlist.append(domain)
+                    domainList.append(domain)
             else:
                 if domains[domain].quickContains(position) == 1:
-                    domainlist.append(domain)
-        return domainlist
+                    domainList.append(domain)
+        return domainList
 
 
