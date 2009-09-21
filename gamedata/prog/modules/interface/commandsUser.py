dc7fcd2a68507fed0d114d9661200d706faf03f1
mogurijin <mogurijin@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2009-09-21 07:55:20 +0000

    I added a few new user commands for the terminal:  * getSetting(key)  * getControl(key)  * listUserCommands()
    I also extended the help function to print the doc strings of commands like so:
    help(listUserCommands)
    
    We now need to create doc strings for all the commands. :D

--- gamedata/prog/modules/interface/commandsUser.py
+++ gamedata/prog/modules/interface/commandsUser.py
@@ -5,10 +5,18 @@ will attempt to send your input as a text message. \
 You can precede your input with a forward slash (/) to \
 explicitly designate your input as a python command (if it \
 fails, it will not be sent as a text message, so you won't be \
-embarassed when you fail)."""
+embarassed when you fail).\n\nUse listUserCommands() and \
+use help(command) to learn more about a specific command"""
 
-def help():
-    output(helpText)
+# ----------------------------
+# Terminal Specific Commands
+# ----------------------------
+
+def help(cmd = None):
+	if cmd:
+		output(cmd.__doc__)
+	else:
+		output(helpText)
 
 def output(s):
     s = str(s)
@@ -22,10 +30,21 @@ def clear():
     import modules
     modules.interface.terminal.clear()
 
+def listUserCommands():
+	"""
+	Usage: listUserCommands()
+		Displays a list of commands available to the user
+	"""
+	import modules
+	for i in dir(modules.interface.commandsUser):
+		if not i.startswith("__"):
+			output(i)
+			
+			
 
-
-
-
+# -----------------
+# Option Commands
+# -----------------
 
 def defaultOptions():
     import modules
@@ -38,12 +57,22 @@ def setSetting(key, value):
     options = modules.interface.options
     r = options.setSetting(key, value)
     output("Success value: %s"%(r))
-
+	
+def getSetting(key):
+	import modules
+	options = modules.interface.options
+	output("Setting %s is set to %s" % (key, options.settings[key]))
+	
 def setControl(key, value):
     import modules
     options = modules.interface.options
     r = options.setControl(key, value)
     output("Success value: %s"%(r))
+	
+def getControl(key):
+	import modules
+	options = modules.interface.options
+	output("Control %s is set to %s" % (key, options.controls[key]))
 
 def loadOptions():
     import modules
