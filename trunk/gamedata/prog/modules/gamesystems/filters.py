923baee750313254703caaee3e9a7c7a2022845a
chasemoskal <chasemoskal@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2009-09-21 07:11:09 +0000

    2D Filter stuff improved. • Corrected an issue where the "Lum" script always ran, slowing down the game even when HDR was not in use. • Externalized "Lum" and removed it from prog by integrating it with "gamesystems/filters.py".

--- trunk/gamedata/prog/modules/gamesystems/filters.py
+++ trunk/gamedata/prog/modules/gamesystems/filters.py
@@ -1,4 +1,4 @@
-### Filters! ###
+### 2D Filters! ###
 
 def run(con):
     """
@@ -16,3 +16,67 @@ def run(con):
             own["HDR"] = False
     else:
         own["HDR"] = False
+
+
+
+
+
+
+
+
+### Lum: A lovely companion to HDR ###
+
+def runLum(con):
+    if con.owner["HDR"]:
+        doLum(con)
+
+def doLum(cont):
+    import GameLogic as G
+    import BGL
+    import Rasterizer as R
+
+    scene = G.getCurrentScene()
+    #cont = G.getCurrentController()
+    objList = scene.objects
+    own = cont.owner
+
+    viewport = BGL.Buffer(BGL.GL_INT, 4)
+    BGL.glGetIntegerv(BGL.GL_VIEWPORT, viewport);
+
+    width = R.getWindowWidth()
+    height = R.getWindowHeight()
+
+    x = viewport[0] + width/2
+    y = viewport[1] + height/2
+
+    x1 = viewport[0] + width/3
+    y1 = viewport[1] + height/3
+
+    x2 = viewport[0] + width/3
+    y2 = viewport[1] + height/2
+
+    x3 = viewport[0] + width/2
+    y3 = viewport[1] + height/2
+
+    x4 = viewport[0] + width/2
+    y4 = viewport[1] + height/3
+
+    pixels = BGL.Buffer(BGL.GL_FLOAT, [1])
+    pixels1 = BGL.Buffer(BGL.GL_FLOAT, [1])
+    pixels2 = BGL.Buffer(BGL.GL_FLOAT, [1])
+    pixels3 = BGL.Buffer(BGL.GL_FLOAT, [1])
+    pixels4 = BGL.Buffer(BGL.GL_FLOAT, [1])
+
+    BGL.glReadPixels(x, y, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixels)
+    BGL.glReadPixels(x1, y1, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixels1)
+    BGL.glReadPixels(x2, y2, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixels2)
+    BGL.glReadPixels(x3, y3, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixels3)
+    BGL.glReadPixels(x4, y4, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixels4)
+
+    avgPixels = (pixels[0]+pixels1[0]+pixels2[0]+pixels3[0]+pixels4[0])/5.0
+
+    #Slow adaptation
+    own.avgL = (own.avgL*0.9 + avgPixels*0.1)
+
+    avgPixels = own.avgL
+
