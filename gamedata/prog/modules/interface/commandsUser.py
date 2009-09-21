5badc7aa6d6307890515166ec428f455a327c871
mogurijin <mogurijin@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2009-09-21 09:31:58 +0000

    Gave a terminal a history so now you can use the up and down arrow keys to go through previous commands. The max history is currently set to 10.

--- gamedata/prog/modules/interface/commandsUser.py
+++ gamedata/prog/modules/interface/commandsUser.py
@@ -5,18 +5,18 @@ will attempt to send your input as a text message. \
 You can precede your input with a forward slash (/) to \
 explicitly designate your input as a python command (if it \
 fails, it will not be sent as a text message, so you won't be \
-embarassed when you fail).\n\nUse listUserCommands() and \
-use help(command) to learn more about a specific command"""
+embarassed when you fail). Use listUserCommands() and \
+use help(command) to learn more about a specific command."""
 
 # ----------------------------
 # Terminal Specific Commands
 # ----------------------------
 
 def help(cmd = None):
-	if cmd:
-		output(cmd.__doc__)
-	else:
-		output(helpText)
+    if cmd:
+        output(cmd.__doc__)
+    else:
+        output(helpText)
 
 def output(s):
     s = str(s)
@@ -31,16 +31,17 @@ def clear():
     modules.interface.terminal.clear()
 
 def listUserCommands():
-	"""
-	Usage: listUserCommands()
-		Displays a list of commands available to the user
-	"""
-	import modules
-	for i in dir(modules.interface.commandsUser):
-		if not i.startswith("__"):
-			output(i)
-			
-			
+    """
+    Usage: listUserCommands()
+        Displays a list of commands available to the user
+    """
+    import modules
+    for i in dir(modules.interface.commandsUser):
+        # Filter out the python module stuff so we only get our commands
+        if not i.startswith("__"):
+            output(i)
+            
+            
 
 # -----------------
 # Option Commands
@@ -57,22 +58,22 @@ def setSetting(key, value):
     options = modules.interface.options
     r = options.setSetting(key, value)
     output("Success value: %s"%(r))
-	
+    
 def getSetting(key):
-	import modules
-	options = modules.interface.options
-	output("Setting %s is set to %s" % (key, options.settings[key]))
-	
+    import modules
+    options = modules.interface.options
+    output("Setting %s is set to %s" % (key, options.settings[key]))
+    
 def setControl(key, value):
     import modules
     options = modules.interface.options
     r = options.setControl(key, value)
     output("Success value: %s"%(r))
-	
+    
 def getControl(key):
-	import modules
-	options = modules.interface.options
-	output("Control %s is set to %s" % (key, options.controls[key]))
+    import modules
+    options = modules.interface.options
+    output("Control %s is set to %s" % (key, options.controls[key]))
 
 def loadOptions():
     import modules
