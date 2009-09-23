fce62fe8a4b04e2009f9cde86d6ee3a582154499
mogurijin <mogurijin@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2009-09-23 02:49:45 +0000

    First commit to test updating and replication of player objects. I can't conduct a good test to see if it's working 100%.

--- trunk/gamedata/prog/modules/gamecontrol/gamestate.py
+++ trunk/gamedata/prog/modules/gamecontrol/gamestate.py
@@ -85,7 +85,7 @@ class GAMESTATE:
 		# Attributes
 		P["A"] = {}
 		P["A"]["P"] = [0.0, 0.0, 0.0]
-		P["A"]["O"] = 0
+		P["A"]["O"] = [0.0, 0.0, 0.0]
 
 		# Server-controlled Attributes
 		P["SA"] = {}
