cfeea61a6622efc15a0865372261bd77976aae73
Geoff.Gollmer <Geoff.Gollmer@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2009-09-26 00:53:50 +0000

    Optimized Luminosity script. It reduces logic by 10-20%.

--- gamedata/prog/modules/gamesystems/filters.py
+++ gamedata/prog/modules/gamesystems/filters.py
@@ -1,11 +1,15 @@
 ### 2D Filters! ###
 
+#HDR globals
+HDRstep = 0
+HDRpixels = [0.0, 0.0, 0.0, 0.0, 0.0]
+
 def run(con):
 	"""
 	Turns filters on and off based on options.settings
 	"""
 	own = con.owner
-	
+
 	import modules
 	options = modules.interface.options
 
@@ -35,8 +39,10 @@ def doLum(cont):
 	import BGL
 	import Rasterizer as R
 
+	global HDRstep
+	global HDRpixels
+
 	scene = G.getCurrentScene()
-	#cont = G.getCurrentController()
 	objList = scene.objects
 	own = cont.owner
 
@@ -46,34 +52,72 @@ def doLum(cont):
 	width = R.getWindowWidth()
 	height = R.getWindowHeight()
 
-	x = viewport[0] + width/2
-	y = viewport[1] + height/2
 
-	x1 = viewport[0] + width/3
-	y1 = viewport[1] + height/3
+	#this calculates the average pixel luminosity, calculating the luminosity
+	#step 0 is an initiation step, it calculates all 5 pixels luminosity to give the filter a start point.
+	if HDRstep == 0 or HDRstep == 1:
+		x = viewport[0] + width/2
+		y = viewport[1] + height/2
 
-	x2 = viewport[0] + width/3
-	y2 = viewport[1] + height/2
+		pixel = BGL.Buffer(BGL.GL_FLOAT, [1])
+		BGL.glReadPixels(x, y, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixel)
 
-	x3 = viewport[0] + width/2
-	y3 = viewport[1] + height/2
+		HDRpixels[0] = pixel[0]
 
-	x4 = viewport[0] + width/2
-	y4 = viewport[1] + height/3
 
-	pixels = BGL.Buffer(BGL.GL_FLOAT, [1])
-	pixels1 = BGL.Buffer(BGL.GL_FLOAT, [1])
-	pixels2 = BGL.Buffer(BGL.GL_FLOAT, [1])
-	pixels3 = BGL.Buffer(BGL.GL_FLOAT, [1])
-	pixels4 = BGL.Buffer(BGL.GL_FLOAT, [1])
 
-	BGL.glReadPixels(x, y, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixels)
-	BGL.glReadPixels(x1, y1, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixels1)
-	BGL.glReadPixels(x2, y2, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixels2)
-	BGL.glReadPixels(x3, y3, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixels3)
-	BGL.glReadPixels(x4, y4, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixels4)
+	if HDRstep == 0 or HDRstep == 2:
+		x = viewport[0] + width/3
+		y = viewport[1] + height/3
 
-	avgPixels = (pixels[0]+pixels1[0]+pixels2[0]+pixels3[0]+pixels4[0])/5.0
+		pixel = BGL.Buffer(BGL.GL_FLOAT, [1])
+		BGL.glReadPixels(x, y, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixel)
+
+		HDRpixels[1] = pixel[0]
+
+
+
+	if HDRstep == 0 or HDRstep == 3:
+		x = viewport[0] + width/3
+		y = viewport[1] + height/2
+
+		pixel = BGL.Buffer(BGL.GL_FLOAT, [1])
+		BGL.glReadPixels(x, y, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixel)
+
+		HDRpixels[2] = pixel[0]
+
+
+
+	if HDRstep == 0 or HDRstep == 4:
+		x = viewport[0] + width/2
+		y = viewport[1] + height/2
+
+		pixel = BGL.Buffer(BGL.GL_FLOAT, [1])
+		BGL.glReadPixels(x, y, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixel)
+
+		HDRpixels[3] = pixel[0]
+
+
+
+	if HDRstep == 0 or HDRstep == 5:
+		x = viewport[0] + width/2
+		y = viewport[1] + height/3
+
+		pixel = BGL.Buffer(BGL.GL_FLOAT, [1])
+		BGL.glReadPixels(x, y, 1, 1, BGL.GL_LUMINANCE, BGL.GL_FLOAT, pixel)
+
+		HDRpixels[4] = pixel[0]
+
+		
+	#step progression
+	if HDRstep < 5:
+		HDRstep += 1
+	else:
+		HDRstep = 1
+
+		
+
+	avgPixels = (HDRpixels[0]+HDRpixels[1]+HDRpixels[2]+HDRpixels[3]+HDRpixels[4])/5.0
 
 	#Slow adaptation
 	eyeAdapt = 0.01
