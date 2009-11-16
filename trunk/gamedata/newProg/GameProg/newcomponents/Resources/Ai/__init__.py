c067887623e83a4364ae9cc5c912fd9d0387b705
chasemoskal <chasemoskal@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2009-11-16 23:52:05 +0000

    Just committing some changes...

---
+++ trunk/gamedata/newProg/GameProg/newcomponents/Resources/Ai/__init__.py
@@ -0,0 +1,7 @@
+### Ai Component ###
+
+class Class:
+	def __init__(self, cont):
+		pass
+	def run(self):
+		pass
--- trunk/gamedata/newProg/GameProg/slab.py
+++ trunk/gamedata/newProg/GameProg/slab.py
@@ -1,4 +1,13 @@
 # The Slab
 """
-A blank module that will be used to store instances of GameProg components.
+Module that will be used to store instances of GameProg components.
+Also contains the master info class/object.
 """
+
+class MasterInfoClass:
+	def __init__(self):
+		self.try_mscon = 0
+		self.try_gpscon = 0
+
+
+MasterInfo = MasterInfoClass()
