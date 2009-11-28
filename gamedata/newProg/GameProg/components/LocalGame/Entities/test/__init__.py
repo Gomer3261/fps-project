8fece3ae72bb86bf287b8f33a39b9ff99c038eb4
Geoff.Gollmer <Geoff.Gollmer@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2009-11-28 23:33:27 +0000

    Added basic functionality to test entity. \Geoff

--- gamedata/newProg/GameProg/components/LocalGame/Entities/test/__init__.py
+++ gamedata/newProg/GameProg/components/LocalGame/Entities/test/__init__.py
@@ -9,6 +9,14 @@ class Class:
 		self.Networking = LocalGame.Networking
 		self.Interface = LocalGame.Interface
 		
+		import GameLogic as gl
+		own = gl.getCurrentController().owner
+		self.obj = gl.getCurrentScene().addObject("test", own)
+		
+		changes = {"T":0}
+		package = ['GS', ['EC', [EID, changes]]]
+		Networking.gpsnet.send(package)
+		
 		print("Test Entity Created\n\n")
 		
 	
@@ -19,13 +27,21 @@ class Class:
 		GameState = self.GameState
 		Networking = self.Networking
 		Interface = self.Interface
-	
-	
+		
 		entityData = GameState.getEntity(EID)
-		entityDataOut = {}
+	
+		if entityData["CD"]["T"] < 50:
+			tick = entityData["CD"]["T"] + 1
+			
+			#updating GameState
+			changes = {"T":tick}
+			package = ['GS', ['EC', [EID, changes]]]
+			Networking.gpsnet.send(package)
+			
+		else:
+			#ending the object
+			package = ['GS', ['AR', ['RE', EID]]]
+			Networking.gpsnet.send(package)
+			self.obj.endObject()
 		
-		print("Running Test Entity")
-		
-		#package = entityDataOut
-		#Networking.gpsnet.send(package) # TCP
-		#Networking.gpsnet.throw(package) # UDP
+		entityData = GameState.getEntity(EID)
