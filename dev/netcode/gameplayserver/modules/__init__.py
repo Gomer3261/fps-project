398e1486846d62c4d582f3b39475708a268e68ac
chasemoskal <chasemoskal@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2010-03-18 05:33:53 +0000

    Spring Cleaning!!!     /Chase.

--- dev/netcode/gameplayserver/modules/__init__.py
+++
@@ -1,8 +0,0 @@
-import comms
-import detailreader
-import gamestate
-import gameplayserver
-import handlers
-import theater
-
-#gamestate = gamestate.GAMESTATE()
--- dev/netcode/masterserver/test.py
+++
@@ -1,4 +0,0 @@
-d = {"a":1, "b":2}
-
-for k, v in d:
-    print key, value
