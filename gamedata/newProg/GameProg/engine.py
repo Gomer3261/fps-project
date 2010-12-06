2d8227f2141d33e4ec7f73583d01b4f3ea2f3a1f
chasemoskal <chasemoskal@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2010-12-06 12:20:53 +0000

    The Rebirth of the FPS Project. engine.py is a start at redoing the whole system, from a high-level point of view, at least. Many components will remain similar or untouched, many concepts remain the same. Following a new philosophy of simplicity, the goal is to try to produce understandable code, and maybe even get something done for once. No promises. /Chase.

---
+++ gamedata/newProg/GameProg/engine.py
@@ -0,0 +1,52 @@
+# Our Game Engine Within A Game Engine.
+
+GameState.host=False # Designates if we are the host.
+GameState.net=True # Designates if this will take place over internet/lan.
+
+if GameState.host and GameState.net: GameState.mode="server"
+if (not GameState.host) and GameState.net: GameState.mode="client"
+if GameState.host and (not GameState.net): GameState.mode="local"
+if (not GameState.host) and (not GameState.net): GameState.mode="replay"
+
+Network.addr = "96.54.129.113"
+Network.port = 3205
+
+
+
+def MainLoop():
+	
+	# Server exclusive routines.
+	if GameState.mode == "server":
+		if not Network.Server:
+			Network.initiateServer(Network.port) # Server initiation.
+		else:
+			newUsers = Network.acceptConnections() # Asyncronously accepts new connections after giving them the GameState and UID.
+			GameState.addUsers( newUsers ) # Adds any new users to the GameState.
+			Network.sendFullGameState( GameState.data, 5.0 ) # Sends out a full gamestate package (sends are reliable UDP)
+			Network.throwDeltaGameState( GameState.deltaData, 0.1 ) # Throws (unreliably transfers over UDP) changes in the GameState to clients.
+	
+	# Client exclusive routines
+	if GameState.mode == "client":
+		if not Network.Client:
+			Network.initiateClient( Network.addr,Network.port )
+	
+	if GameState.mode=="client" or GameState.mode=="server":
+		messages = Network.catchMessages()
+		if messages: Network.addToInBuffer(messages)
+	
+	### UNIVERSAL ROUTINES
+	# These apply to server, client, and local modes.
+	
+	GameState.interpret( Network.inBuffer ) # The GameState interprets incoming messages.
+	
+	LocalGame.conform( GameState.data ) # LocalGame emulates the GameState by adding objects or removing them based on what the GameState says.
+	
+	for entity in LocalGame.entities: # We loop through every entity.
+		entity.conform( GameState.data ) # Each entity conforms to the GameState as it sees fit.
+		if entity.getMode() == "control": # Only control entities send info to the gamestate (to request changes)
+			messages, reliable = entity.run() # Running controlled entities.
+			if GameState.host:
+				Network.addToInBuffer(messages) # Running a control entity generates data output, which we send to the GameState via Network, as always. In this case, we inject the output messages directly to the network's input stream, so GameState will get it next tick, with Network.feedback().
+			else:
+				if reliable: Network.send(messages)
+				else: Network.throw(messages)
