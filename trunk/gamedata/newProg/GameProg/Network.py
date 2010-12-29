b831f4a45ee88513df52ecf0309bac013a44939b
Geoff.Gollmer <Geoff.Gollmer@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2010-12-29 03:18:16 +0000

    chase: totally added stuff.totally.

--- trunk/gamedata/newProg/GameProg/Network.py
+++ trunk/gamedata/newProg/GameProg/Network.py
@@ -2,9 +2,43 @@ Server=None # Server Object.
 Client=None # Client Object.
 inBuffer=[] # List of messages that Network has recieved.
 
+
+
+
 class initiateServer:
 	def __init__(self,port):
-		pass
+		import socket
+		self.buf=1024
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.sock.bind(('',port))
+		
+		connections = {} # Dictionary of connections
+		
+		#self.nextTicket=0
+	#def getTicket(self): t=self.nextTicket; self.nextTicket+=1; return t
+	
+	class makeConnection:
+		def __init__(self,addr):
+			#self.ticket=ticket
+			self.addr=addr
+			self.throwInSeq=0; self.throwOutSeq=0
+			self.sendInSeq=0; self.sendOutSeq=0
+	
+	def lowLevelStuff(self):
+		import netfuncs
+		data,addr = self.sock.recvfrom(self.buf)
+		if data:
+			if data[0]=="\x11": # It's a network message.
+				if data[1] == "c": # they want to initiate a connection.
+					self.connections[addr] = self.makeConnection(addr)
+					packet = netfuncs.makeNetPacket('a', newTicket, payload):
+					self.sock.sendto(packet,addr)
+
+
+
+
+
+
 
 class initiateClient:
 	def __init__(self,addr,port):
@@ -22,6 +56,7 @@ def catchMessages():
 	return messages
 
 def addToInBuffer(messages):
+	global inBuffer
 	for message in messages:
 		inBuffer.append(message)
 
---
+++ trunk/gamedata/newProg/GameProg/netfuncs.py
@@ -0,0 +1,12 @@
+
+
+def makeNetPacket(type, payload):
+	p="\x11"
+
+
+def makeThrowPacket(type, seq, payload):
+	p="\x12"
+
+
+def makeSendPacket(type, seq, payload):
+	p="\x13"
