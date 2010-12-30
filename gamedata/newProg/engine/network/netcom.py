aa3996b5fdd6754ba4d644b8e4343ceb0deddf21
Geoff.Gollmer <Geoff.Gollmer@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2010-12-30 09:20:09 +0000

    :)

--- gamedata/newProg/engine/network/netcom.py
+++ gamedata/newProg/engine/network/netcom.py
@@ -7,47 +7,43 @@ def unpack(packet):
 	return pickle.loads( packet )
 
 
-
-
-
-
-
 #######
-def serverBuildThrowPacket(seq, payload):
-	packet = codes['throw']
-	packet+= bytes(seq) + codes['sep']
-	packet+= pickle.dumps( payload )
-	return packet
 
-def serverParseThrowPacket(packet):
-	flag = packet[0]
-	maindata = packet[1:]
-	seqStr, idStr, payload = maindata.split( codes['sep'] )
-	seq=int(seqStr); id=int(idStr)
-	return seq, id, pickle.loads( payload )
-
-
-def clientBuildThrowPacket(seq, id, payload):
-	packet = codes['throw']
-	packet+= bytes(seq) + codes['sep']
-	packet+= bytes(id) + codes['sep']
-	packet+= pickle.dumps( payload )
-	return packet
-
-
-def clientParseThrowPacket(packet):
-	flag = packet[0]
-	maindata = packet[1:]
-	seqStr, payload = maindata.split( codes['sep'] )
-	seq=int(seqStr)
-	return seq, pickle.loads( payload )
-
-
-
+#def serverBuildThrowPacket(seq, payload):
+#	packet = codes['throw']
+#	packet+= bytes(seq) + codes['sep']
+#	packet+= pickle.dumps( payload )
+#	return packet
+#
+#def serverParseThrowPacket(packet):
+#	flag = packet[0]
+#	maindata = packet[1:]
+#	seqStr, idStr, payload = maindata.split( codes['sep'] )
+#	seq=int(seqStr); id=int(idStr)
+#	return seq, id, pickle.loads( payload )
+#
+#
+#def clientBuildThrowPacket(seq, id, payload):
+#	packet = codes['throw']
+#	packet+= bytes(seq) + codes['sep']
+#	packet+= bytes(id) + codes['sep']
+#	packet+= pickle.dumps( payload )
+#	return packet
+#
+#
+#def clientParseThrowPacket(packet):
+#	flag = packet[0]
+#	maindata = packet[1:]
+#	seqStr, payload = maindata.split( codes['sep'] )
+#	seq=int(seqStr)
+#	return seq, pickle.loads( payload )
 
 
-def sendStream():
-	pass
 
-def recvStream():
-	pass
+
+
+#def sendStream():
+#	pass
+
+#def recvStream():
+#	pass
