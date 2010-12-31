67c5c4f2693396975fe0c04b15939e25560b59a2
chasemoskal <chasemoskal@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2010-12-31 02:27:47 +0000

    added camera system, have no cameras in the game with which to test it :P

---
+++ trunk/gamedata/newProg/engine/camera.py
@@ -0,0 +1,24 @@
+### Camera System ###
+
+import bge
+scene = bge.logic.getCurrentScene()
+
+eCam = scene.objects['eCam']
+scene.active_camera = eCam
+currentCameraPriority = 0
+
+# Every camera will call this each tick, and they will fight for supremacy
+def offer(camera, priority):
+	global scene, eCam, currentCameraPriority
+	if camera != scene.active_camera:
+		if priority > currentCameraPriority:
+			scene.active_camera = camera
+			currentCameraPriority = priority
+
+def reset():
+	global scene, eCam, currentCameraPriority
+	scene.active_camera = eCam
+
+def resetPriority():
+	global currentCameraPriority
+	currentCameraPriority = 0
