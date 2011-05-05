3cef97febd888ed6c1b3f06bb5926c87b9bbedba
chasemoskal <chasemoskal@f3498f2e-9c4c-11de-b0ac-87f46fbf356c>
2011-05-05 09:15:08 +0000

    Tweaked the way Entities work. /Chase

--- trunk/gamedata/newProg/engine/entities/director.py
+++ trunk/gamedata/newProg/engine/entities/director.py
@@ -2,43 +2,30 @@
 import engine.entities.baseEntity as baseEntity
 class Class(baseEntity.Class):
 
-	def simulateServerData(self, gamestate):
-		"""
-		Simulates stuff, and returns gamestate delta data to the
-		mainloop, where it is merged with the gamestate delta.
-		"""
-		deltas = []
-		
+	##################
+	###### HOST ###### Server-side behaviour for this entity.
+	################## Defines server-data; handles memos.
+	def host(self, gamestate):	
 		import engine
 		import bge
 		keyboard = bge.logic.keyboard
 		
 		if keyboard.events[bge.events.PKEY] == 3 and (not engine.interface.terminalIsActive()):
 			if not gamestate.hasEntity(engine.id, 'player'): 
-				deltas.append( {'E':{gamestate.getNextId():{'t':'player','c':engine.id}}} )
-		
-		for memo in self.memos:
+				self.submitDelta( {'E':{gamestate.getNextId():{'t':'player','c':engine.id}}} )
+	
+	def hostHandleMemos(self):
+		for memo in self.memoInbox:
 			type, controlId = memo
 			if type=='player' and gamestate.hasEntity(controlId, type): continue
-			deltas.append( {'E':{gamestate.getNextId():{'t':type,'c':controlId}}} )
-		self.memos = [] # Clearing memos.
-		
-		return deltas # Return delta data to be merged with gamestate.delta
+			self.submitDelta( {'E':{gamestate.getNextId():{'t':type,'c':controlId}}} )
 	
-	def replicateServerData(self, gamestate):
-		memos = []
-		
+	####################
+	###### CLIENT ###### Client-side behaviour for this entity.
+	#################### Replicates server-data.
+	def client(self, gamestate):		
 		import engine
 		import bge
 		keyboard = bge.logic.keyboard
 		if keyboard.events[bge.events.PKEY] == 3 and (not engine.interface.terminalIsActive()):
-			memos.append( (self.id, ('player', self.engine.id)) )
-		
-		return memos # Return memos.
-	
-	def simulateControllerData(self, gamestate):
-		"""
-		Simulates stuff, and returns gamestate delta data to the
-		mainloop, where it is merged with the gamestate delta.
-		"""
-		return [] # Return delta data to be merged with gamestate.delta
+			self.submitMemo( (self.id, ('player', self.engine.id)) )
